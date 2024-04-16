942bad14c7d3a582205889cd6886fd96e21c5f90
pedromendonca-dev <pedrolucasmacm@gmail.com>
2024-04-16 01:35:40 -0300

    uppload files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="app">
+    <BrowserRouter>
+    <Navbar/>
+    <div className="container">
+    <Routes>
+
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+
+    </Routes>
     </div>
+    <Footer/>
+    </BrowserRouter>
+   </div>
   );
 }
 
---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsjdaSLeXYkZd5vrFqHNwV8pKDx3_QN2M",
+  authDomain: "miniblog-ddfee.firebaseapp.com",
+  projectId: "miniblog-ddfee",
+  storageBucket: "miniblog-ddfee.appspot.com",
+  messagingSenderId: "1047923592972",
+  appId: "1:1047923592972:web:c0db302e2224c4c0d4c329"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
